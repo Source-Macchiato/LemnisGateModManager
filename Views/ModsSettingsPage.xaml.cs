84d9ef5732659c29baf2e2d7c795669032a39944
Alyx <50267518+Xyla-TV@users.noreply.github.com>
2025-02-15 18:56:26 +0100

    started code for pick a folder, have to finish it

--- Views/ModsSettingsPage.xaml.cs
+++ Views/ModsSettingsPage.xaml.cs
@@ -1,31 +1,61 @@
 using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Controls.Primitives;
-using Microsoft.UI.Xaml.Data;
-using Microsoft.UI.Xaml.Input;
-using Microsoft.UI.Xaml.Media;
-using Microsoft.UI.Xaml.Navigation;
+using Windows.Storage;
+using Windows.Storage.AccessCache;
+using Windows.Storage.Pickers;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
 
 namespace LemnisGateLauncher.Views
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
     public sealed partial class ModsSettingsPage : Page
     {
         public ModsSettingsPage()
         {
             this.InitializeComponent();
+        }
+
+        private async void PickFolderButton_Click(object sender, RoutedEventArgs e)
+        {
+            //disable the button to avoid double-clicking
+            var senderButton = sender as Button;
+            senderButton.IsEnabled = false;
+
+            // Clear previous returned file name, if it exists, between iterations of this scenario
+            PickFolderOutputTextBlock.Text = "";
+
+            // Create a folder picker
+            FolderPicker openPicker = new FolderPicker();
+
+            // See the sample code below for how to make the window accessible from the App class.
+            var window = this;
+
+            // Retrieve the window handle (HWND) of the current WinUI 3 window.
+            var hWnd = WinRT.Interop.WindowNative.GetWindowHandle(window);
+
+            // Initialize the folder picker with the window handle (HWND).
+            WinRT.Interop.InitializeWithWindow.Initialize(openPicker, hWnd);
+
+            // Set options for your folder picker
+            openPicker.SuggestedStartLocation = PickerLocationId.Desktop;
+            openPicker.FileTypeFilter.Add("*");
+
+            // Open the picker for the user to pick a folder
+            StorageFolder folder = await openPicker.PickSingleFolderAsync();
+            if (folder != null)
+            {
+                StorageApplicationPermissions.FutureAccessList.AddOrReplace("PickedFolderToken", folder);
+                PickFolderOutputTextBlock.Text = "Picked folder: " + folder.Name;
+            }
+            else
+            {
+                PickFolderOutputTextBlock.Text = "Operation cancelled.";
+            }
+
+            //re-enable the button
+            senderButton.IsEnabled = true;
         }
     }
 }
