e75f4fd862a52c70171f1b57938748bc1c8c7976
Alyx <50267518+Xyla-TV@users.noreply.github.com>
2025-02-26 12:32:28 +0100

    system for save and load settings

--- App.axaml.cs
+++ App.axaml.cs
@@ -1,11 +1,38 @@
+using System.IO;
 using Avalonia;
 using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Markup.Xaml;
+using Microsoft.Extensions.Configuration;
 
 namespace LemnisGateLauncher;
 
 public partial class App : Application
 {
+    public static App? Instance { get; private set; }
+    private readonly IConfiguration? _configuration;
+
+    public App()
+    {
+        Instance = this;
+
+        string configFilePath = Path.Combine(Directory.GetCurrentDirectory(), "appsettings.json");
+
+        if (!File.Exists(configFilePath))
+        {
+            var defaultConfig = new
+            {
+                GamePath = ""
+            };
+            string defaultConfigJson = Newtonsoft.Json.JsonConvert.SerializeObject(defaultConfig, Newtonsoft.Json.Formatting.Indented);
+            File.WriteAllText(configFilePath, defaultConfigJson);
+        }
+
+        _configuration = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+            .Build();
+    }
+
     public override void Initialize()
     {
         AvaloniaXamlLoader.Load(this);
@@ -19,5 +46,23 @@ public partial class App : Application
         }
 
         base.OnFrameworkInitializationCompleted();
+    }
+
+    public void SaveSelectedFolderPath(string path)
+    {
+        var json = File.ReadAllText("appsettings.json");
+        dynamic? jsonObj = Newtonsoft.Json.JsonConvert.DeserializeObject(json);
+
+        if (jsonObj != null)
+        {
+            jsonObj["GamePath"] = path;
+            string output = Newtonsoft.Json.JsonConvert.SerializeObject(jsonObj, Newtonsoft.Json.Formatting.Indented);
+            File.WriteAllText("appsettings.json", output);
+        }
+    }
+
+    public string? LoadSavedFolderPath()
+    {
+        return _configuration?["GamePath"];
     }
 }
