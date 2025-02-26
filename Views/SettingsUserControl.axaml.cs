7fdd78228c56a556463ce8227b4373948c1d58da
Alyx <50267518+Xyla-TV@users.noreply.github.com>
2025-02-26 12:32:47 +0100

    system for select folder and save path

--- Views/SettingsUserControl.axaml.cs
+++ Views/SettingsUserControl.axaml.cs
@@ -1,6 +1,7 @@
-using Avalonia;
+using System.Threading.Tasks;
 using Avalonia.Controls;
-using Avalonia.Markup.Xaml;
+using Avalonia.Interactivity;
+using Avalonia.Platform.Storage;
 
 namespace LemnisGateLauncher.Views;
 
@@ -8,6 +9,38 @@ public partial class SettingsUserControl : UserControl
 {
     public SettingsUserControl()
     {
-        InitializeComponent();
+        InitializeComponent();        
+    }
+
+    private async void OnSelectFolderButton_Click(object sender, RoutedEventArgs e)
+    {
+        var window = this.VisualRoot as Window;
+        
+        if (window != null)
+        {
+            var folder = await SelectFolderAsync(window);
+
+            if (!string.IsNullOrEmpty(folder))
+            {
+                App.Instance?.SaveSelectedFolderPath(folder);
+            }
+        }
+    }
+
+    private async Task<string?> SelectFolderAsync(Window window)
+    {
+        var options = new FolderPickerOpenOptions
+        {
+            Title = "Select Folder"
+        };
+
+        var result = await window.StorageProvider.OpenFolderPickerAsync(options);
+
+        if (result.Count > 0)
+        {
+            return result[0].Path.LocalPath;
+        }
+
+        return null;
     }
 }
